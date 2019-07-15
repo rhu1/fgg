07f7239ceb629a3c0e2b6c2eff1590c9c976db5f
rhu1 <r.z.h.hu@herts.ac.uk>
2019-07-15 19:50:55 +0100

    Minor.

--- fg/examples/hello.go
+++ fg/examples/hello.go
@@ -4,7 +4,7 @@
 // N.B. FG (or at least this implementation) is intended to be white-space *insensitive*.
 // (E.g., this program could be written all on one line.)
 // So the ';' are mandatory -- cannot replace by newlines (as in actual Go).
-// (Newlines and whitespace may be freely added, though.)
+// (Newlines and other whitespace may be freely added, though.)
 package main;
 type A struct{};
 func (x0 A) m() A { return x0.m() };
