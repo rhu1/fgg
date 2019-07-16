1adcc0623ba9ddf810939ca133c382d569f28968
rhu1 <r.z.h.hu@herts.ac.uk>
2019-07-16 14:06:09 +0100

    Minor.

--- fg/examples/stupidcast/stupidcast.go
+++ fg/examples/stupidcast/stupidcast.go
@@ -1,5 +1,5 @@
 //$ go run github.com/rhu1/fgg -eval=-1 -v fg/examples/stupidcast/stupidcast.go
-//C f.
+// Cf.
 //$ go run github.com/rhu1/fgg/fg/examples/stupidcast
 
 package main; type Any interface{}; type ToAny struct { any Any }; type A struct {}; type B struct{}; func main() { _ = ToAny{A{}}.any.(B) }
