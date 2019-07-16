2939bc8deb61456c7c3a3b7ba90ae1a9efcaa862
rhu1 <r.z.h.hu@herts.ac.uk>
2019-07-16 09:41:45 +0100

    Minor.

--- fg/examples/booleans/booleans.go
+++ fg/examples/booleans/booleans.go
@@ -20,10 +20,10 @@ type Branches interface {
 type TT struct{};
 type FF struct{};
 
-func (this TT) Not() Bool            { return FF{} };
-func (this FF) Not() Bool            { return TT{} };
-func (this TT) Equal(that Any) Bool  { return that.(Bool) };
-func (this FF) Equal(that Any) Bool  { return that.(Bool).Not() };
+func (this TT) Not() Bool { return FF{} };
+func (this FF) Not() Bool { return TT{} };
+func (this TT) Equal(that Any) Bool { return that.(Bool) };
+func (this FF) Equal(that Any) Bool { return that.(Bool).Not() };
 func (this TT) Cond(br Branches) Any { return br.IfTT() };
 func (this FF) Cond(br Branches) Any { return br.IfFF() };
 
