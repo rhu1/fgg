3a5df071adc351443b8281cb948aebc78a6016a2
rhu1 <r.z.h.hu@herts.ac.uk>
2019-09-17 13:26:54 +0100

    [WIP] Continued: adding RepDecls to MDecls.

--- fgr/fgr_translation.go
+++ fgr/fgr_translation.go
@@ -11,7 +11,7 @@ import (
 var _ = fmt.Errorf
 
 /*HERE
-- do assert->ifthenelse trans
+- getTypeReps
 - add meth-param RepDecls
 - FGR eval
 */
@@ -47,8 +47,9 @@ func Translate(p fgg.FGGProgram) FGRProgram { // TODO FIXME: FGR -- TODO also ca
 			e_getv := NewStructLit(Type("Dummy_0"), []Expr{})
 			t := Type(d1.GetName())
 			getv := NewMDecl(NewParamDecl("x", t), "getValue",
+				[]RepDecl{}, // FIXME
 				[]ParamDecl{}, Type("Any_0"), e_getv)
-			// gettr := ...TODO...
+			// gettr := ...TODO FIXME...
 
 			ds_fgr = append(ds_fgr, s, getv)
 		case fgg.ITypeLit:
@@ -77,8 +78,9 @@ func Translate(p fgg.FGGProgram) FGRProgram { // TODO FIXME: FGR -- TODO also ca
 		// TODO: factor out with STypeLits
 		e_getv := NewSelect(NewVariable("x"), "value") // CHECKME: but t_S doesn't have value field, wrapper does?
 		getv := NewMDecl(NewParamDecl("x", Type(k)), "getValue",
+			[]RepDecl{}, // FIXME
 			[]ParamDecl{}, Type("Any_0"), e_getv)
-		// gettr := ...TODO...
+		// gettr := ...TODO FIXME...
 		ds_fgr = append(ds_fgr, adptr, getv)
 
 		// Add duck-typing meths
@@ -125,6 +127,7 @@ func Translate(p fgg.FGGProgram) FGRProgram { // TODO FIXME: FGR -- TODO also ca
 			//e = NewAssert(e, toFgTypeFromBounds(delta, u))
 			e = addGetValueCast(delta, e, u)
 			md := NewMDecl(NewParamDecl("x", k), g.GetName(),
+				[]RepDecl{}, // FIXME
 				pds, t, e)
 			ds_fgr = append(ds_fgr, md)
 			//}
@@ -237,7 +240,9 @@ func fgrTransMDecl(ds []Decl, d1 fgg.MDecl, wrappers map[Type]adptrPair) MDecl {
 	}
 	e = e.Subs(subs)
 
-	return NewMDecl(recv, m, pds, t, e)
+	return NewMDecl(recv, m,
+		[]RepDecl{}, // FIXME
+		pds, t, e)
 }
 
 /* Expr */
