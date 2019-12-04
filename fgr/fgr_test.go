b55430eafa7279f4fdcc46d8a45e05d732ea4e45
rhu1 <r.z.h.hu@herts.ac.uk>
2019-12-04 16:59:31 +0000

    Minor.

--- fgr/fgr_test.go
+++ fgr/fgr_test.go
@@ -4,14 +4,14 @@
 
 package fgr_test // Separate package, can test "API"
 
-import (
+/*import (
 	"fmt"
 	"testing"
 
 	"github.com/rhu1/fgg/base"
 	"github.com/rhu1/fgg/base/testutils"
 	"github.com/rhu1/fgg/fgr"
-)
+)*/
 
 /* Harness funcs * /
 
@@ -31,22 +31,22 @@ func parseAndOkBad(t *testing.T, msg string, elems ...string) base.Program {
 
 // TOOD: make translation to FGG and compare results to -fgg
 
-func Test001(t *testing.T) {
-	/*A := "type A struct {}"
+/*func Test001(t *testing.T) {
+	A := "type A struct {}"
 	Am1 := "func (x0 A) m1() A { return x0 }"
 	Am2 := "func (x0 A) m2(x1 A) A { return x1 }"
 	Am3 := "func (x0 A) m3(x1 A, x2 A) A { return x2 }"
 	B := "type B struct { f A }"
 	e := "B{A{}}"
-	parseAndOkGood(t, A, Am1, Am2, Am3, B, e)*/
-}
+	parseAndOkGood(t, A, Am1, Am2, Am3, B, e)
+}*/
 
 /* Eval */
 
-func TestEval001(t *testing.T) {
-	/*A := "type A struct {}"
+/*func TestEval001(t *testing.T) {
+	A := "type A struct {}"
 	B := "type B struct { f A }"
 	e := "B{A{}}.f"
 	prog := parseAndOkGood(t, A, B, e)
-	testutils.EvalAndOkGood(t, prog, 1)*/
-}
+	testutils.EvalAndOkGood(t, prog, 1)
+}*/
