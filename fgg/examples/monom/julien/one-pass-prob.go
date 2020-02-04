98735451290af5c82efe6e935549232ebfeb7309
Julien Lange <j.s.lange@kent.ac.uk>
2020-02-04 10:58:04 +0000

    add example re: one pass with meth-name-driven monom

---
+++ fgg/examples/monom/julien/one-pass-prob.go
@@ -0,0 +1,37 @@
+package main;
+
+type Any(type ) interface {};
+
+type Int(type ) struct {};
+type Bool(type ) struct {};
+
+type Pair(type a Any(), b Any() ) struct { 
+	fst a;
+	snd b
+};
+
+type IA(type ) interface{
+	MyFunction(type b Any())(y b) Int()
+};
+
+type SA(type ) struct {};
+ 
+// NB: SA.MyFunction() is only called via interface, 
+// can find the instantiation of Pair(Int,Int) in one pass?
+func (x SA(type )) MyFunction(type b Any())(y b) Int() {return Pair(Int(),Int()){Int(){}, Int(){}}.snd};
+
+
+type Dummy(type ) struct{};
+
+func (x Dummy(type )) CallFunction(type )(y IA()) Int() {
+	return y.MyFunction(Int())(Int(){})
+};
+
+
+func main() { _ =
+	Dummy(){}.CallFunction()(SA(){})
+
+
+}
+
+
