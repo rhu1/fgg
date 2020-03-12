f9de02eb5d5ebc1669896ebcbc6d43c831e5ce8e
Julien Lange <j.s.lange@kent.ac.uk>
2020-03-12 18:09:36 +0000

    add example via struct

---
+++ fgg/examples/monom/julien/struct-poly-rec.go
@@ -0,0 +1,17 @@
+// This is not monomorphisable 
+package main;
+
+type Any(type ) interface {};
+
+type B(type a Any()) struct {val C(a)};
+
+type C(type a Any()) struct {};
+
+type A(type ) struct {};
+
+func (x B(type a Any())) m(type )() B(C(a)) {
+	return B(C(a)){C(C(a)){}}
+};
+
+
+func main() { _ =  B(A()){C(A()){}}}
