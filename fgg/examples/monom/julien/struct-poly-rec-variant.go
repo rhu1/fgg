2e06b545f92c504a19fb1dfe3fa4f148efc04bb9
Julien Lange <j.s.lange@kent.ac.uk>
2020-03-17 14:34:28 +0000

    edit example with recursive struct

--- fgg/examples/monom/julien/struct-poly-rec-variant.go
+++ fgg/examples/monom/julien/struct-poly-rec-variant.go
@@ -5,11 +5,13 @@ type Any(type ) interface {};
 
 type A(type ) struct {};
 
-type BS(type a Any()) struct {};
+type B(type a Any()) struct {val C(a)};
 
-func (x BS(type a Any())) m(type )() BS(BS(a)) {
-	return BS(BS(a)){}
+type C(type a Any()) struct {val B(B(a))};
+
+func (x A(type )) m(type a Any())() B(a) {
+	return A(){}.m(a)()
 };
 
 
-func main() { _ =  BS(A()){}.m()()}
+func main() { _ =  A(){}.m(A())() }
