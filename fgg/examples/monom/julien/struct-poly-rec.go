46b22061cdc5004b6ca99ad0ce0e511c2a2444e5
Julien Lange <j.s.lange@kent.ac.uk>
2020-03-18 16:50:38 +0000

    add iface example

--- fgg/examples/monom/julien/struct-poly-rec.go
+++ fgg/examples/monom/julien/struct-poly-rec.go
@@ -20,6 +20,12 @@ func main() { _ =  A(){}.m(A())() }
 
 for all: 
 type t(type ) struct T, 
-then t never appears in T (inter-procedurally)
+then t never appears in fields*(T) (transitively / inter-procedurally for struct)
 
 */
+
+type Box(type a Any()) struct {val IBox(a)};
+type IBox(type a Any()) interface { box() IBox(IBox(a)); unbox() a};
+
+type Cons(a Any()) struct { head a; tail List(a)};
+type Nil(a Any()) struct { };
