df20ff8931bb516070f140590fb7135c1e324c71
Julien Lange <j.s.lange@kent.ac.uk>
2020-03-18 14:13:42 +0000

    add comments

--- fgg/examples/monom/julien/i-closure-bad.go
+++ fgg/examples/monom/julien/i-closure-bad.go
@@ -7,8 +7,9 @@ type Dummy(type ) struct {};
 
 type Pair(type ) struct {fst Any(); snd Any()};
 
+// we need IB <: IA here
 func (x Dummy(type )) useInterfaceA(type )(y IA()) IA() {
-	return y.m1(S())()
+	return y.m1(S())() // m1() returns IB
 };
 
 func (x Dummy(type )) useInterfaceB(type )(y IB()) Any() {
@@ -16,7 +17,9 @@ func (x Dummy(type )) useInterfaceB(type )(y IB()) Any() {
 };
 
 type IA(type ) interface { m1(type a Any())() IB()};
-type IB(type ) interface { m1(type a Any())() IB();  m2(type a Any())() IB() }; // IB <: IA
+type IB(type ) interface { 
+	m1(type a Any())() IB();   // IB.m1() never occurs => need I-closure()
+	m2(type a Any())() IB() }; // IB <: IA
 
 type S(type ) struct {};
 func (x S(type )) m1(type a Any())() IB() {return S(){}};
