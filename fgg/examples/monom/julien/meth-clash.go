747ba8ff4322192da4c174fa07e554f5dbe9b578
Julien Lange <j.lange@ic.ac.uk>
2020-04-06 15:34:21 +0100

    add comments

---
+++ fgg/examples/monom/julien/meth-clash.go
@@ -0,0 +1,44 @@
+package main;
+
+type Any(type ) interface {};
+
+type Top(type ) interface {};
+
+
+type Dummy(type ) struct {};
+
+func (x Dummy(type )) toAny(type )(y Any()) Any() {
+	return y.(Any())
+};
+
+type Triple(type ) struct{ fst Any(); snd Any(); thd Any()};
+
+type Foo(type ) interface { m(type a Top())() Dummy() };
+
+// S </: Foo
+type S(type ) struct {};
+func (x S(type )) m(type a Any())() Dummy() {
+	return Dummy(){}
+};
+
+
+
+// T <: Foo
+
+type T(type ) struct {};
+func (x T(type )) m(type a Top())() Dummy() {
+	return Dummy(){}
+};
+
+func main() { _ =  
+	// Here both S.m() and Foo.m()  -which dont have matching signatures-
+	// are instantiated to the same type (S).
+	// which leads to mono'd FGG to accept a cast that fails in FGG
+	Triple(){
+		S(){}.m(S())(),
+		//
+		Dummy(){}.toAny()(T(){}).(Foo()).m(S())(),  // type-assert succeeds
+		//
+		Dummy(){}.toAny()(S(){}).(Foo()) // type-assert fails
+	}
+	}
