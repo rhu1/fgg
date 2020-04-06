0a50e55e464d9ee960433388120b6017c3bfe425
Julien Lange <j.lange@ic.ac.uk>
2020-04-06 13:05:52 +0100

    add example for bad cast with param methods

---
+++ fgg/examples/monom/julien/param-meth-cast.go
@@ -0,0 +1,26 @@
+// This illustrates the need to preserve the full method sets when monomorphising
+package main;
+
+type Any(type ) interface {};
+
+type S(type ) struct {};
+
+type T(type ) struct {};
+
+type Foo(type ) interface { m(type a Any())(x a, y S) Any() };
+
+type Bar(type ) interface { m(type a Any())(x a, y T) Any() };
+
+type V(type ) struct {};
+
+func (x V(type )) toAny(type )(y Foo()) Any() {
+	return y.(Any())
+};
+
+// V <: Foo 
+func (x V(type )) m(type a Any())(x a, y S) Any() {
+	return x.(Any())
+};
+
+func main() { _ =  V(){}.toAny()(V(){}).(Bar()) }
+
