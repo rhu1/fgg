98c2b08a404f2eea0dd1f6b68a20a9bfb888ab3a
Julien Lange <j.s.lange@kent.ac.uk>
2020-05-12 18:34:46 +0100

    add examples

---
+++ fgg/examples/monom/julien/mono-ok/no-compo.go
@@ -0,0 +1,34 @@
+package main;
+
+
+type Any(type ) interface {};
+
+type Pair(type ) struct { fst Any(); snd Any()};
+
+type tI(type a Any()) interface {
+	m(type )() Any()
+};
+
+type A(type ) struct {};
+
+func (x A(type )) m(type )() Any() {
+	return A(){}
+};
+
+type B(type ) struct {};
+func (x B(type )) m(type )() Any() {
+	return B(){}
+};
+
+type C(type ) struct {};
+func (x C(type )) mtop(type )(y tI()) Any() {
+	return y.m()()
+};
+
+
+func main() { _ =
+	Pair(){
+		C(){}.mtop()(A(){})
+		, B(){}
+	}
+}
