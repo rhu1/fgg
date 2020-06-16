91d870c3ee5be82ab735259f24d75c4a02ea2b04
rhu1 <r.z.h.hu@herts.ac.uk>
2020-06-16 16:30:05 +0100

    Fixed conficts in examples.

--- fgg/examples/monom/misc/mono-ok/Parameterised-Map.go
+++ fgg/examples/monom/misc/mono-ok/Parameterised-Map.go
@@ -1,5 +1,6 @@
 package main;
 
+import "fmt";
 
 type Any(type ) interface {};
 
@@ -11,34 +12,36 @@ type Func(type a Any(), b Any()) interface {
 };
 
 type Bool2Int(type ) struct {};
+type ParamBox(type a Any()) struct {v1 a};
+func (x ParamBox(type a Any())) map(type b Any())(f Func(a,b)) Box(b) {return ParamBox(b){f.apply()(x.v1)}};
 
-func (x Bool2Int(type )) apply(type )(x Bool()) Int() {return Int(){} };
+
+func (x Bool2Int(type )) apply(type )(y Bool()) Int() {return Int(){} };
 
 type Box(type a Any()) interface{
 	map(type b Any())(f Func(a,b)) Box(b)
 
 };
 
-type IntBox(type ) struct {v Int()}; // IntBox <: 
-func (x IntBox(type )) map(type b Any())(f Func(Int(),b)) Box(b) {return BoolBox{f.apply()(x.v)}}; 
+type IntBox(type ) struct {v1 Int()}; // IntBox <:
+func (x IntBox(type )) map(type b Any())(f Func(Int(),b)) Box(b) {return ParamBox(b){f.apply()(x.v1)}};
 
 
-type BoolBox(type ) struct {v Bool()}; // BoolBox <: IA(Bool())
-func (x BoolBox(type )) map(type b Any())(f Func(Bool(),b)) Box(b) {return IntBox{f.apply()(x.v)}}; 
-
+type BoolBox(type ) struct {v1 Bool()}; // BoolBox <: IA(Bool())
+func (x BoolBox(type )) map(type b Any())(f Func(Bool(),b)) Box(b) {return ParamBox(b){f.apply()(x.v1)}};
 
 
 type Dummy(type ) struct{};
 
-func (x Dummy(type )) CallFunctionBool(type )(y Box(Bool())) Bool() {
-	return y.map(Int())(Bool2Int(){}) 
+func (x Dummy(type )) CallFunctionBool(type )(y Box(Bool())) Box(Int()) {
+	return y.map(Int())(Bool2Int(){})
 
 };
 
 
-func main() { _ =
-	Dummy(){}.CallFunctionBool()(BoolBox(){})  
-
+func main() {
+	//_ =
+	fmt.Printf("%#v",
+		Dummy(){}.CallFunctionBool()(BoolBox(){Bool(){}})
+	)
 }
-
-
