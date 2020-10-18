8838ead2d2b792bed4acc459df339295f687fc39
rhu1 <r.z.h.hu@herts.ac.uk>
2020-10-18 21:44:25 +0100

    Minor example tidying.

---
+++ examples/fg/hello/fmtprintf/fmtprintf.go
@@ -0,0 +1,15 @@
+//$ go run github.com/rhu1/fgg -v -eval=10 fg/examples/hello/fmtprintf.go
+// Cf.
+//$ go run github.com/rhu1/fgg/fg/examples/hello
+
+// N.B. FG (or at least this implementation) is intended to be white-space *insensitive*.
+// (E.g., this program could be written all on one line.)
+// So the ';' are mandatory -- cannot replace by newlines (as in actual Go).
+// (Newlines and other whitespace may be freely added, though.)
+package main;
+import "fmt";
+type World struct{};
+func (x0 World) hello() World { return x0.hello() };
+func main() {
+	fmt.Printf("%#v", World{}.hello())
+}
